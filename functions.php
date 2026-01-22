19de14937dd1aafc92e988e0d6d8292c8640d12a
Helder <helder@redesoftware.com.br>
2026-01-22 10:30:56 -0300

    estilo: Remover breadcrumb de todas as páginas fix: Criar versões para ao arquivo style.css para evitar problemas de caching ao carregar o estilo

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
 <?php
 function storely_child_enqueue_styles() {
     wp_enqueue_style('storely-parent-style', get_template_directory_uri() . '/style.css');
-//     wp_enqueue_style('storely-child-style', get_stylesheet_uri(), array('storely-parent-style'));
 	
-	 wp_enqueue_style('child-style',
-        get_stylesheet_uri() . '/style.css',
-        ['storely-parent-stylestyle'], // Make sure it loads after parent
-        wp_get_theme()->get('Version')
+	 wp_enqueue_style(
+		'child-style',
+        get_stylesheet_uri(),
+        ['storely-parent-stylestyle'], // Carrega primeiro o estilo do tema pai, depois o tema filho
+        filemtime(get_stylesheet_directory() . '/style.css')
     );
 }
-add_action('wp_enqueue_scripts', 'storely_child_enqueue_styles');
+add_action('wp_enqueue_scripts', 'storely_child_enqueue_styles', 20);
