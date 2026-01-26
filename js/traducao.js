0a03d1da3597032b28943a97eeebf07afb451452
Helder <helder@redesoftware.com.br>
2026-01-26 10:39:58 -0300

    fix: Adicionar tradução do placeholder da barra de pesquisa

--- js/traducao.js
+++ js/traducao.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-// 	
+// 	Tradução do botão de navegação de categorias
     const btn = document.querySelector('.product-category-btn');
     if (!btn) return;
 	
@@ -8,10 +8,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     span.childNodes.forEach(node => {
         if (node.nodeType === Node.TEXT_NODE) {
-            node.textContent = ' Buscar Categorias';
+            node.textContent = ' Todas as Categorias';
         }
     });
 	
 	btn.style.visibility = 'visible';
-// 	
+// 
+	
+// 	Tradução do placeholder da barra de pesquisa
+	const headerSearchInput = document.querySelector('.header-search-input');
+	headerSearchInput.setAttribute('placeholder', "Buscar Produtos");
 })
