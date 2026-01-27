d643b1472865476574a280821e02d925209da406
Helder <helder@redesoftware.com.br>
2026-01-27 16:08:05 -0300

    style: Remover caixa de seleção e barra de pesquisa quando o usuário está na página 'Carrinho' ou em 'Minha Conta'

--- functions.php
+++ functions.php
@@ -20,12 +20,16 @@ function storely_child_enqueue_styles() {
 }
 
 function storely_child_enqueue_scripts() {
-	wp_enqueue_script(
-		'traducao-js',
-		get_stylesheet_directory_uri() . '/js/traducao.js',
-		[],
-		'1.0',
-		true);
+	if(!is_shop()) {
+		wp_enqueue_script(
+			'traducao-js',
+			get_stylesheet_directory_uri() . '/js/traducao.js',
+			[],
+			'1.0',
+			true
+		);
+	}
+	
 	
 	if(is_shop()) {
 		wp_enqueue_script(
@@ -33,7 +37,15 @@ function storely_child_enqueue_scripts() {
 			get_stylesheet_directory_uri() . '/js/card-product.js',
 			[],
 			'1.0',
-			true);
+			true
+		);
+	}
+
+	// Remove a caixa de seleção de categorias e a barra de pesquisa
+	// quando o usuário está na página "Carrinho", ou em "Minha Conta"
+	if(is_cart() || is_account_page()) {
+		remove_action('storely_hdr_browse_cat', 'storely_hdr_browse_cat');
+		remove_action('storely_hdr_product_search', 'storely_hdr_product_search');
 	}
 }
 
